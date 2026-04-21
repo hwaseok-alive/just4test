697800614f3b6b2f2ed79ecc4e41d51abdeb2111
hwaseok-alive <alsdk7805@gmail.com>
2026-04-21 21:23:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,51 +10,44 @@ function closeDetail() {
 }
 
 function switchTab(el, name) {
-    // 탭 상태 업데이트
     document.querySelectorAll('.tab').forEach(t => t.classList.remove('active'));
     el.classList.add('active');
 
     const cleanName = name.replace('◈ ', '');
     const infoPanel = document.getElementById('info-panel');
-    const titleElement = document.getElementById('info-title');
     const imgContainer = document.getElementById('image-container');
+    const titleElement = document.getElementById('info-title');
     
-    titleElement.innerText = cleanName;
     imgContainer.innerHTML = ''; 
     infoPanel.classList.remove('hidden-block');
+    titleElement.innerText = cleanName;
 
-    // 탭별 분기
     if (cleanName === '표지') {
-        // 표지: 중앙 이미지 표시 + 우측 블록 삭제
         imgContainer.innerHTML = `<img src="https://i.imgur.com/ad45cb.jpg" class="content-img">`;
         infoPanel.classList.add('hidden-block');
     } 
     else if (cleanName === '속성') {
-        // 속성: 캐릭터 이미지 표시 + 우측 블록 유지
         imgContainer.innerHTML = `<img src="https://i.imgur.com/l03409J.png" class="content-img">`;
     } 
     else if (cleanName === '물품') {
-        // 물품: 순차 애니메이션 카드 + 우측 블록 삭제
         infoPanel.classList.add('hidden-block');
         imgContainer.innerHTML = `
             <div class="item-list">
-                <img src="https://i.imgur.com/UBQIzgK.png" class="item-card" id="c1">
-                <img src="https://i.imgur.com/UZkRJ1V.png" class="item-card" id="c2">
-                <img src="https://i.imgur.com/Za0T3xy.png" class="item-card" id="c3">
+                <img src="https://i.imgur.com/UBQIzgK.png" class="item-card" id="i1">
+                <img src="https://i.imgur.com/UZkRJ1V.png" class="item-card" id="i2">
+                <img src="https://i.imgur.com/Za0T3xy.png" class="item-card" id="i3">
             </div>
         `;
-        setTimeout(() => document.getElementById('c1').classList.add('animate'), 100);
-        setTimeout(() => document.getElementById('c2').classList.add('animate'), 300);
-        setTimeout(() => document.getElementById('c3').classList.add('animate'), 500);
+        setTimeout(() => document.getElementById('i1').classList.add('animate'), 100);
+        setTimeout(() => document.getElementById('i2').classList.add('animate'), 300);
+        setTimeout(() => document.getElementById('i3').classList.add('animate'), 500);
     } 
     else if (cleanName === '문화') {
-        // 문화: 모두 삭제
         infoPanel.classList.add('hidden-block');
     }
 
-    // 애니메이션 리셋
     const box = document.getElementById('info-anim-box');
     box.style.animation = 'none';
     void box.offsetWidth; 
-    box.style.animation = 'charFade 0.5s ease-out';
+    box.style.animation = 'charFade 0.4s ease-out';
 }
